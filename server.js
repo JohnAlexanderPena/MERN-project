f823b629b98541dbc2e535e24f5f2e93e448e335
John <jpena91@gmail.com>
2019-10-11 16:08:06 -0400

    fix bcrypt issue and post request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
@@ -7,6 +8,9 @@ const posts = require('./routes/api/posts')
 
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require('./config/keys').mongoURI
@@ -23,7 +27,7 @@ app.get('/', (req, resp) => resp.send('Hello World'));
 app.use('/api/users', users);
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
-
+ //test
 
 const port = process.env.PORT || 5000;
 
