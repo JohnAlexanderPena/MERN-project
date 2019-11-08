7e5a3c9204b00148b039a3bf3a9bb23ecf652e79
John <jpena91@gmail.com>
2019-11-07 23:52:34 -0500

    create post routes and add add comment/like feature

--- models/Post.js
+++ models/Post.js
@@ -1,51 +1,54 @@
 const mongoose = require('mongoose');
-const scheme = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 //Create Schema
 const PostSchema = new Schema({
   user: {
-    type: Schema,
+    type: Schema.Types.ObjectId,
     ref: 'users',
   },
   text: {
-  type:String,
-  require: true
+    type:String,
+    require: true
   },
   name: {
-    type: String
+    type: String,
+    require: true
   },
   avatar: {
     type: String
   },
   likes: [ // keep track of duplicate likes
+  {
     user: {
-      type: Schema,
-      ref: 'users',
-    }
-  ],
-  comments: [
-    {
-      user: { //comments will have use
-        type: Schema,
+        type: Schema.Types.ObjectId,
         ref: 'users',
-      },
-      text: {
-        type: String,
-        required: true
-      },
-      avatar: {
-        type: String
-      },
-      date: {
-        type: Date,
-        default: Date.now
       }
     }
   ],
+  comments: [
+  {
+    user: { //comments will have use
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+      },
+    text: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
+],
   date: {
    type: Date,
    default: Date.now
   }
 });
 
-moddule.exports = Post = mongoose.model("post", PostSchema)
+module.exports = Post = mongoose.model('post', PostSchema)
