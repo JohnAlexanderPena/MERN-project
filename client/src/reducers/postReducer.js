fee4e32297c4d69ff7199019049fc5a8ae8186e6
John <jpena91@gmail.com>
2019-11-19 00:24:18 -0500

    start post components and setup post form, setup reducer, actions, and setup post POST request

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,11 +1,20 @@
+import { ADD_POST } from '../actions/types'
+
 const initialState = {
   posts: [],
   post: {},
   loading: false
 }
 
+
+
 export default function(state = initialState, action) {
-  switch(action) {
+  switch(action.type) {
+    case ADD_POST:
+    return {
+      ...state,
+      posts:[action.payload, ...state.posts] //adding new post coming in from payload automatically
+    }
     default:
     return state;
   }
