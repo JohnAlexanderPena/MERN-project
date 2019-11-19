8621188a219552b3d9041e4d5c37a25172e413f6
John <jpena91@gmail.com>
2019-11-19 15:31:05 -0500

    add like and unlike functionality to post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,16 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import PostForm from './PostForm'
+import PostFeed from './PostFeed'
 import Spinner from '../common/Spinner'
+import { getPosts } from '../../actions/postActions'
 
 class Posts extends React.Component {
+
+  componentDidMount() {
+    this.props.getPosts(); // fetch posts
+  }
   render () {
+    const { posts, loading } = this.props.post;
+    //initialize post content
+    let postContent
+
+    //check is posts is null to setup our spinner function
+    if(posts === null || loading) {
+      postContent = <Spinner />
+    } else {
+      postContent = <PostFeed posts={posts} />
+    }
+
     return (
       <div className="feed" >
           <div className="container">
             <div className="row">
               <div className="col-md-12">
                 <PostForm/>
+                {postContent}
               </div>
             </div>
           </div>
@@ -20,4 +38,12 @@ class Posts extends React.Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  post: state.post
+})
+export default connect(mapStateToProps, { getPosts })(Posts);
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST } from '../actions/types'
+import { ADD_POST, GET_POSTS, POST_LOADING, DELETE_POST } from '../actions/types'
 
 const initialState = {
   posts: [],
@@ -10,10 +10,26 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch(action.type) {
+    case POST_LOADING:
+    return {
+      ...state,
+      loading: true //adding new post coming in from payload automatically
+    }
+    case GET_POSTS:
+    return {
+      ...state,
+      posts: action.payload,
+      loading: false //Stop spinner
+    }
     case ADD_POST:
     return {
       ...state,
       posts:[action.payload, ...state.posts] //adding new post coming in from payload automatically
+    }
+    case DELETE_POST:
+    return {
+      ...state,
+      posts: state.posts.filter(post => post._id !== action.payload), // remove post that matches id and returns other posts
     }
     default:
     return state;
