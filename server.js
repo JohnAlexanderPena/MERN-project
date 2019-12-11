55d121db5b6f6db38f4e7f682f7d66f2baa4334f
John <jpena91@gmail.com>
2019-12-10 19:28:56 -0500

    watev

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.use('/api/posts', posts)
    res.sendFile(path.join(__dirname, './public/index.html'));
  })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
